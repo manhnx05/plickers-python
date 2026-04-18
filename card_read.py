6cdd3b0d575781d305ab98ba283cf0467e268c7f
Nguyễn Xuân Mạnh <nguyenxuanmanh918@gmail.com>
2026-04-19 03:09:58 +0700

    Upgrade card_read.py to Python 3

--- card_read.py
+++ card_read.py
@@ -4,7 +4,7 @@ import numpy as np
 from my_math import  Math
 from time import sleep
 import os
-
+import sys
 import scipy
 
 
@@ -12,7 +12,7 @@ import scipy
 def card_read(img):
     dst = cv2.blur(img,(3,3))
     gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
-    np.set_printoptions(threshold='nan')
+    np.set_printoptions(threshold=sys.maxsize)
 
 
     #gray = cv2.imread('img.jpg', 0)
@@ -22,15 +22,15 @@ def card_read(img):
     #nimg, contours, h = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
         if len(cnt) >500:
-            #print 'now',cnt
+            #print('now',cnt)
             approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
-            # print len(approx)
+            # print(len(approx))
             if len(approx) > 11:
-                print 'square'
-                print 'cnt len',len(cnt)
-                print 'shape',cnt.shape
-                print 'x max ',cnt[:,:,0].max(),'x min',cnt[:,:,0].min()
-                print cnt[:,:,0].mean()
+                print('square')
+                print('cnt len',len(cnt))
+                print('shape',cnt.shape)
+                print('x max ',cnt[:,:,0].max(),'x min',cnt[:,:,0].min())
+                print(cnt[:,:,0].mean())
                 x_leter,x_biger,y_leter,y_biger=[],[],[],[]
 
                 for i in  cnt[:,:,0].tolist():
@@ -50,7 +50,7 @@ def card_read(img):
                 x_leter_mode=Math.mode(x_leter)
                 y_biger_mode=Math.mode(y_biger)
                 y_leter_mode=Math.mode(y_leter)
-                print x_biger_mode,x_leter_mode
+                print(x_biger_mode,x_leter_mode)
                 #a,b,c,d= cnt[0][0],cnt[int(len(cnt)*0.33)][0],cnt[len(cnt)*0.66][0],cnt[-1][0] #
 
                 #card=gray[y_leter_mode[0]:y_biger_mode[0],x_leter_mode[0]:x_biger_mode[0]]
@@ -59,13 +59,13 @@ def card_read(img):
                 wigth,higth=card.shape
                 #cv2.imshow('card',card)
                 #cv2.waitKey(0)
-                print wigth,higth,type(wigth),type(higth)
+                print(wigth,higth,type(wigth),type(higth))
                 count=1
                 card_array=np.zeros([5,5]) #array sort [y,x]
                 for y in range(1,6):
                     for x in range(1,6):
                         back_while=0
-                        print 'now local',int(higth*y/5.0),int(wigth*x/5.0)
+                        print('now local',int(higth*y/5.0),int(wigth*x/5.0))
                         if y ==1 and x==1:
                             crop=result[0:int(higth*(y/5.0)),0:int(wigth*(x/5.0))]
                         elif y ==1:
@@ -83,13 +83,13 @@ def card_read(img):
                             card_array[y-1,x-1]=1
                         else:
                             card_array[y-1,x-1]=0
-                        print 'con',count,y,x,back_while,np.average(crop)
+                        print('con',count,y,x,back_while,np.average(crop))
                         count+=1
                 return card_array
 
 import pickle
 if __name__ == '__main__':
-    file_list =os.listdir('/home/project/camera/card_file/')
+    file_list =os.listdir('./card_file/')
     card_data=[]
     card_list=[]
     for file in file_list:
@@ -100,8 +100,8 @@ if __name__ == '__main__':
 
         card_array=card_read(img)
 
-        print 'file',file_num,option
-        print card_array
+        print('file',file_num,option)
+        print(card_array)
         if option=='A':
             A=card_array
             B=np.rot90(card_array,1)
