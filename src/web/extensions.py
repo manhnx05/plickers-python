714d2c6ed5bf499075d3abede7a8b1554cead214
Nguyễn Xuân Mạnh <nguyenxuanmanh918@gmail.com>
2026-06-28 23:42:13 +0700

    Extract Flask extensions to prevent circular imports.

---
+++ src/web/extensions.py
@@ -0,0 +1,10 @@
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from src.core.models import User
+
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
